c276890c70447c540a0c5899367c385005799dc7
Mister-J <stuckonbasketball@gmail.com>
2018-08-17 15:59:11 -0700

    Update 8/17
    Changed the html for the index page.

--- index.php
+++ index.php
@@ -1,22 +1,64 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
 	<title>GRBC Library</title>
+	
+	<!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-		<div class="topbar">
-			<img src="images/logoplaceholder.png" class="logo">
-			<button type="submit" name="submit" class="navbutton" onclick="home()">Home</button>	
-		</div>
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/agency.css" rel="stylesheet">
 </head>
 
-<body>
+<body id="page-top">
 
-	<link href="css/stylesheet.css" type="text/css" rel="stylesheet"/>
 
-	<button onclick="checkout();" class="homeButton button1" id="checkout" type="button">Check Out Book</button>
-	<button onclick="view();" class="homeButton button2" id="view" type="button">View Inventory</button>
+<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="index.php">Home</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Support</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-	<a href="login.php" class="adminlink">Log in as Admin</a>
+<!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Welcome To The Library!</div>
+          <!--<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>!-->
+		  <button onclick="checkout();" class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" id="checkout" type="button">Check Out Book</button>
+		  <button onclick="view();" class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" id="view" type="button">View Inventory</button>
+        </div>
+      </div>
+    </header>
+
+	<!--<a href="login.php" class="adminlink">Log in as Admin</a>!-->
 
 	<script>
 		function checkout()
@@ -34,5 +76,19 @@
 			window.location='index.php';
 		}
 	</script>
+	
+	<!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/agency.min.js"></script>
 </body>
 </html>
